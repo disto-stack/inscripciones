feb97000aa4354a032fab574cf61e268a12da840
Juan Velasquez <juan.1701812756@ucaldas.edu.co>
2022-11-18 15:36:17 -0500

    primer commit

---
+++ inscripciones/LectorArchivo.java
@@ -0,0 +1,18 @@
+package inscripciones;
+
+import java.util.List;
+
+/**
+ * LectorArchivo
+ */
+public class LectorArchivo {
+
+    /* Constructor */
+    public void LectorArchivo() {
+
+    }
+
+    public List<String> leer(String ruta) {
+        return null;
+    }
+}
